ffbeac075c8fecffd1dadfb02408089c46cb9bc5
lil Eliel <eliechamakone@gmail.com>
2025-11-13 22:39:21 -0500

    Mise en place du projet et implémentation des classes User et authority

---
+++ src/test/java/ca/uqac/groupe/examgu/ExamGuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uqac.groupe.examgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGuAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
