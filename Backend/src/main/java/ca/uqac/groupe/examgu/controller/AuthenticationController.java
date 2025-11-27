10fe1f8e95287404915fa199eb3fcc680b1b4295
lil Eliel <eliechamakone@gmail.com>
2025-11-26 20:10:27 -0500

    correction des import

--- Backend/src/main/java/ca/uqac/groupe/examgu/controller/AuthenticationController.java
+++ Backend/src/main/java/ca/uqac/groupe/examgu/controller/AuthenticationController.java
@@ -5,11 +5,11 @@ import ca.uqac.groupe.examgu.request.RegisterRequest;
 import ca.uqac.groupe.examgu.response.AuthenticationResponse;
 import ca.uqac.groupe.examgu.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,16 +26,18 @@ public class AuthenticationController {
     public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
-@Operation(summary ="Register a user ", description ="Create new user in database " )
+
+    @Operation(summary ="Register a user ", description ="Create new user in database " )
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
     public void register(@Valid @RequestBody RegisterRequest registerRequest) throws Exception {
-authenticationService.register(registerRequest);
+        authenticationService.register(registerRequest);
     }
+
     @Operation(summary ="Login a user", description ="submit email & password to authenticate user ")
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
-    public AuthenticationResponse login(@Valid @RequestBody AuthenticationRequest authRequest )  {
+    public AuthenticationResponse login(@Valid @RequestBody AuthenticationRequest authRequest)  {
         return authenticationService.login(authRequest);
     }
 }
