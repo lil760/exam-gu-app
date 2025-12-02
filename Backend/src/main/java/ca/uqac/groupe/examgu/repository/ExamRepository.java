4280c1a7a1853ea272bf2fb60ab4203f409d76a2
lil Eliel <eliechamakone@gmail.com>
2025-12-01 20:05:32 -0500

    modification de la gestion des  accès

--- Backend/src/main/java/ca/uqac/groupe/examgu/repository/ExamRepository.java
+++ Backend/src/main/java/ca/uqac/groupe/examgu/repository/ExamRepository.java
@@ -3,5 +3,13 @@ package ca.uqac.groupe.examgu.repository;
 import ca.uqac.groupe.examgu.entity.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+    List<Exam> findByStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqual(
+            LocalDateTime currentTime1,
+            LocalDateTime currentTime2
+    );
 }
